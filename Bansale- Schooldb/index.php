5aa9758284bfe909dfe7d903d3e63a4b5d2831d0
vinzkirk <82029968+vinzkirk@users.noreply.github.com>
2021-11-19 14:53:04 +0800

    Add files via upload

--- Bansale- Schooldb/index.php
+++ Bansale- Schooldb/index.php
@@ -1,8 +1,41 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <title>Add Records in Database</title>
+</head>
+<body>
+
 <?php
-session_start();
-if(!empty($_SESSION["userId"])) {
-    require_once './view/dashboard.php';
-} else {
-    require_once './view/login-form.php';
+include_once "schooldb.php"; 
+
+if(isset($_POST['submit']))
+{		
+    $fullname = $_POST['fullname'];
+    $age = $_POST['age'];
+
+    $insert = mysqli_query($db,"INSERT INTO tblemp`(fullname`, age) VALUES ('$fullname','$age')");
+
+    if(!$insert)
+    {
+        echo mysqli_error($conn);
+    }
+    else
+    {
+        echo "Records added successfully.";
+    }
 }
+
+mysqli_close($conn); 
 ?>
+
+<h3>Fill the Form</h3>
+
+<form method="POST">
+  Full Name : <input type="text" name="fullname" placeholder="Enter Full Name" Required>
+  <br/>
+  Age : <input type="text" name="age" placeholder="Enter Age" Required>
+  <br/>
+  <input type="submit" name="submit" value="Submit">
+</form>
+
+</body>
